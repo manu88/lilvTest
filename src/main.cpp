c8d919bc340054e5ede9363d9f04c55101eba5b9
manuel deneu <manuel.deneu@gmail.com>
2025-05-22 17:38:29 +0200

    add macos target

--- src/main.cpp
+++ src/main.cpp
@@ -2,37 +2,53 @@
 #include <lilv/lilv.h>
 #include <stdio.h>
 
-static void printPluginInfo(const LilvPlugin *p) {
-  const LilvNode *nodeURI = lilv_plugin_get_uri(p);
-  const LilvNode *nodeName = lilv_plugin_get_name(p);
-  printf("URI: '%s' Name: '%s'\n", lilv_node_as_uri(nodeURI),
-         lilv_node_as_string(nodeName));
+#ifdef MACOS
+#define LV2_PATH "/opt/homebrew/lib/lv2/"
+#else
+#define LV2_PATH "/usr/local/lib/aarch64-linux-gnu/lv2"
+#endif
 
-  const LilvNodes* features = lilv_plugin_get_supported_features(p);
+static void printPluginInfos(const LilvPlugin *p) {
+    LilvNode *nodeName = lilv_plugin_get_name(p);
+    const LilvPluginClass *nodeClass = lilv_plugin_get_class(p);
+    const LilvNode *nodeClassLabel = lilv_plugin_class_get_label(nodeClass);
+    printf("plugin name = '%s' class '%s'\n", lilv_node_as_string(nodeName),
+           lilv_node_as_string(nodeClassLabel));
+    lilv_node_free(nodeName);
+
+    LilvNodes *features = lilv_plugin_get_supported_features(p);
+    LILV_FOREACH(nodes, fI, features) {
+        const LilvNode *nodeFeat = lilv_nodes_get(features, fI);
+        const char *featName = lilv_node_as_string(nodeFeat);
+        printf("\tfeat: '%s'\n", featName);
+    }
+    lilv_nodes_free(features);
 }
 
 int main() {
-  printf("Test Lilv\n");
-  LilvWorld *world = lilv_world_new();
+    printf("Test Lilv\n");
+    LilvWorld *world = lilv_world_new();
 
-  LilvNode *lv2_path =
-      lilv_new_string(world, "/usr/local/lib/aarch64-linux-gnu/lv2");
+    LilvNode *lv2_path =
+        lilv_new_string(world, LV2_PATH);
 
-  lilv_world_set_option(world, LILV_OPTION_LV2_PATH, lv2_path);
-  lilv_world_load_all(world);
+    lilv_world_set_option(world, LILV_OPTION_LV2_PATH, lv2_path);
+    lilv_world_load_all(world);
 
-  const LilvPlugins *plugins = lilv_world_get_all_plugins(world);
+    const LilvPlugins *plugins = lilv_world_get_all_plugins(world);
 
-  const LilvPlugin *pluginToTest = nullptr;
-  LILV_FOREACH(plugins, i, plugins) {
-    const LilvPlugin *p = lilv_plugins_get(plugins, i);
-    printPluginInfo(p);
-    pluginToTest = p;
-  }
+    const LilvPlugin *pluginToTest = nullptr;
+    LILV_FOREACH(plugins, i, plugins) {
+        const LilvPlugin *p = lilv_plugins_get(plugins, i);
+        printPluginInfos(p);
+        pluginToTest = p;
+    }
 
-  LilvInstance *instance = lilv_plugin_instantiate(pluginToTest, 48000.0, NULL);
-  assert(instance);
 #if 0
+    LilvInstance *instance =
+        lilv_plugin_instantiate(pluginToTest, 48000.0, NULL);
+    assert(instance);
+
   printf("Test load specific plugin\n");
   LilvNode *bundle_uri =
       lilv_new_file_uri(world, NULL, "/usr/lib/lv2/atom.lv2");
@@ -40,6 +56,6 @@ int main() {
   printf("bundle ptr %p\n", bundle_uri);
   lilv_world_load_bundle(world, bundle_uri);
 #endif
-  lilv_world_free(world);
-  return 0;
+    lilv_world_free(world);
+    return 0;
 }
